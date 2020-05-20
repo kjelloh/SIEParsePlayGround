9ca53f68e3b69e47c61ed4d12787115710957010
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2020-05-20 17:48:15 +0200

    Refactored SIE content output formatting to procedure
    Output formatting enhanced and put into separate procedure

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,30 @@
 #include <filesystem>
 #include <string>
 
+auto format_and_output_to_cout = [](char ch) -> void {
+       if (ch == 10) { // SIE file defined new-line control character
+            std::cout << "<" << static_cast<int>(ch) << ">" << '\n'; // Show and perform new-line
+        }
+       else if (ch == 13) { // SIE-file NOT new-line but allowed
+            std::cout << "<" << static_cast<int>(ch) << ">"; // Show but do NOT perform new line
+        }
+        else if (    (ch >= ' ')
+                  && (ch <= '~')) { // 7 Bit ASCII
+            std::cout << ch;
+        }
+        else { // Control character
+            std::cout << "<" << static_cast<int>(ch) << ">" << ch;
+        }
+};
+
 int main(int argc, const char * argv[]) {
-    std::filesystem::path sie_file_path("/Users/kjell-olovhogdal/Resilio Sync/XCode/MySIEParser/sie/sie/2326 ITFied 1505-1604.se");
+    std::filesystem::path sie_file_path("/Users/kjell-olovhogdal/Documents/Github/SIEParsePlayGround/sie/2326 ITFied 1505-1604.se");
     std::ifstream sie_file(sie_file_path);
     char ch;
     sie_file.get(ch);
     while (sie_file.good()) {
-        if (ch == 10) { // SIE file defined new-line control character
-            std::cout << "\n";
-        }
-        else if (    (ch >= ' ')
-                  && (ch <= '}')) { // visible character
-            std::cout << ch;
-        }
-        else { // Control character
-            std::cout << "<" << static_cast<int>(ch) << ">";
-        }
-        sie_file.get(ch);
+        format_and_output_to_cout(ch);
+        sie_file.get(ch); // next
     }
 
     std::cout << '\n';
